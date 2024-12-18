2df21af97278e62ca019f172bec14f4bfca7d1ea
HilaScharf <27hscharf@gannacademy.org>
2024-12-18 16:29:02 -0500

    C:\Users\Hila (Local)\Documents\GitHub\processing-project-classes-HilaScharf\src\App.java
    newest version

--- src/App.java
+++ src/App.java
@@ -13,12 +13,15 @@ public class App extends PApplet {
 
     public void setup() {
         background(0);
-        for (int y = 5; y < 100; y += 25) {
-            for (int x = 0; x <= 770; x += 35) {
-                Blocks temp = new Blocks(x, y, 1, this);
-                blocks.add(temp);
+        if (gameStart == 1) {
+            for (int y = 5; y < 100; y += 25) {
+                for (int x = 0; x <= 770; x += 35) {
+                    Blocks temp = new Blocks(x, y, 1, this);
+                    blocks.add(temp);
+                }
             }
         }
+
     }
 
     public void settings() {
@@ -26,24 +29,28 @@ public class App extends PApplet {
     }
 
     public void draw() {
-        while (gameStart == 0) {
-            fill(100, 100, 100);
-            rect(200, 100, 50, 100);
+        if (gameStart == 0) {
             homeScreen();
         }
-        while (gameStart == 1) {
+        if (gameStart == 1) {
             gameplay();
         }
-        while(gameStart == 2) {
+        if (gameStart == 2) {
             endScreen();
+        }
+        if (gameStart == 3) {
+            tutorialScreen();
         }
     }
 
     public void gameplay() {
         background(0);
+        stroke(0);
+        strokeWeight(1);
         Balls ball = new Balls(200, 300, this, 1);
         balls.add(ball);
         ball.make();
+        ball.move();
         paddle(balls);
         int x = 0;
         while (x < blocks.size()) {
@@ -59,7 +66,14 @@ public class App extends PApplet {
             }
             x++;
         }
-        if(balls.size() == 0) {
+        int ballIndex = 0;
+        if (ballIndex < balls.size()) {
+            Balls thisBall = balls.get(ballIndex);
+            if (thisBall.y() > height) {
+                balls.remove(thisBall);
+            }
+        }
+        if (balls.size() == 0) {
             gameStart = 2;
         }
     }
@@ -83,8 +97,63 @@ public class App extends PApplet {
     }
 
     public void homeScreen() {
-
+        fill(0);
+        stroke(255);
+        strokeWeight(5);
+        rect(250, 100, 300, 100);
+        rect(250, 250, 300, 100);
+        rect(250, 400, 300, 100);
+        fill(255);
+        textSize(50);
+        text("new game", 290, 160);
+        text("saved game", 280, 310);
+        text("how to play", 275, 460);
+        if (newGame(250, 100) == true) {
+            gameStart = 1;
+        }
+        if(tutorial() == true) {
+            gameStart = 3;
+        }
     }
+    public void tutorialScreen() {
+        background(0);
+        strokeWeight(4);
+        textSize(25);
+        fill(255);
+        text("use the paddle to prevent the ball from hitting the bottom, and to", 50, 50);
+        text("get the ball to hit and destroy the bricks. use the paddle by moving", 50, 80);
+        text("your mouse. the goal of the game is to destroy all the bricks.", 50, 110);
+        text("destroy the powerup bricks to increase the strength of the ball", 50, 140);
+        stroke(255);
+        strokeWeight(5);
+        fill(0);
+        rect(250, 200, 300, 100);
+        rect(250, 350, 300, 100);
+        fill(255);
+        textSize(50);
+        text("new game", 290, 260);
+        text("saved game", 280, 410);
+        if (newGame(250, 200) == true) {
+            gameStart = 1;
+        }
+    }
+    public boolean tutorial() {
+        if (mousePressed && mouseX >= 250 && mouseX <= 550 && mouseY >= 400 && mouseY <= 500) {
+            println("Rectangle clicked!");
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean newGame(int buttonX, int buttonY) {
+        if (mousePressed && mouseX >= buttonX && mouseX <= buttonX + 300 && mouseY >= buttonY && mouseY <= buttonY + 100) {
+            println("Rectangle clicked!");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public void endScreen() {
 
     }
